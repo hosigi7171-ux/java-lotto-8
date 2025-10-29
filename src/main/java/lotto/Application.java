a9bf489d46c467fef2602a5c8aec0e04bbbd481a
hosigi7171-ux <hosigi7171@gmail.com>
2025-10-29 14:33:08 +0900

    feat: add initial structure and entry point for Lotto game

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,15 @@
 package lotto;
 
+import lotto.controller.LottoController;
+import lotto.service.LottoServiceImpl;
+import lotto.view.ConsoleInputView;
+import lotto.view.ConsoleOutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        LottoController lottoController = new LottoController(new ConsoleInputView(), new ConsoleOutputView(),
+                new LottoServiceImpl());
+
+        lottoController.runGame();
     }
 }
