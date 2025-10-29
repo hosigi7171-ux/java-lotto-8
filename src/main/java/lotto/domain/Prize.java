efc568ff44bb14285052bac3b75b734f0dba94aa
hosigi7171-ux <hosigi7171@gmail.com>
2025-10-29 20:15:37 +0900

    feat(WinningNumbers): Add Prize enum and implement lotto number matching including bonus number check
    - add Prize enum
    - implement lotto number matching

--- src/main/java/lotto/domain/Prize.java
+++ src/main/java/lotto/domain/Prize.java
@@ -1,4 +1,46 @@
 package lotto.domain;
 
 public enum Prize {
+    FIRST(6, false, 2_000_000_000L),
+    SECOND(5, true, 30_000_000L),
+    THIRD(5, false, 1_500_000L),
+    FOURTH(4, false, 50_000),
+    FIFTH(3, false, 5_000);
+
+    private final int matchedNumberCount;
+    private final boolean isMatchedBonusNumber;
+    private final long prizeMoney;
+
+    Prize(int matchedNumberCount, boolean isMatchedBonusNumber, long prizeMoney) {
+        this.matchedNumberCount = matchedNumberCount;
+        this.isMatchedBonusNumber = isMatchedBonusNumber;
+        this.prizeMoney = prizeMoney;
+    }
+
+    public int getMatchedNumberCount() {
+        return matchedNumberCount;
+    }
+
+    public boolean isMatchedBonusNumber() {
+        return isMatchedBonusNumber;
+    }
+
+    public long getPrizeMoney() {
+        return prizeMoney;
+    }
+
+    /**
+     * 일치하는 숫자 개수에 해당하는 enum 을 리턴해주는 메서드
+     *
+     * @param matchedNumberCount 당첨로또와 일치하는 숫자 개수
+     * @return 몇 등상
+     */
+    public static Prize getMatchedPrize(int matchedNumberCount) {
+        for (Prize prize : Prize.values()) {
+            if (prize.getMatchedNumberCount() == matchedNumberCount) {
+                return prize;
+            }
+        }
+        return null;
+    }
 }
--- src/main/java/lotto/domain/WinningNumbers.java
+++ src/main/java/lotto/domain/WinningNumbers.java
@@ -1,5 +1,9 @@
 package lotto.domain;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class WinningNumbers {
     private final Lotto winningLotto;
     private final BonusNumber bonusNumber;
@@ -13,13 +17,83 @@ public class WinningNumbers {
         this.bonusNumber = bonusNumber;
     }
 
-    private void validate(BonusNumber bonusNumber){
+    private void validate(BonusNumber bonusNumber) {
         validateNotDuplicate(bonusNumber);
     }
 
     private void validateNotDuplicate(BonusNumber bonusNumber) {
-        if(winningLotto.contains(bonusNumber.getValue())){
+        if (winningLotto.contains(bonusNumber.getValue())) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 중복되면 안됩니다.");
         }
+    }
+
+    /**
+     * 로또들에 대해서 일치하는 등수들을 체크해서 결과를 알아내는 메서드
+     *
+     * @param lottos 확인할 로또들
+     * @return 키: 몇 등, 값: 횟수, 등수별로 몇회 해당하는지
+     */
+    public Map<Prize, Integer> match(List<Lotto> lottos) {
+        Map<Prize, Integer> countResult = new HashMap<>();
+        for (Lotto lotto : lottos) {
+            Prize prize = getPrizeForLotto(lotto);
+            // 등수에 해당하면
+            if (prize != null) {
+                plusCount(prize, countResult);
+            }
+        }
+        return countResult;
+    }
+
+    /**
+     * 로또에 해당하는 등수를 찾아주는 메서드
+     *
+     * @param lotto 확인할 로또
+     * @return 해당하는 등수 enum
+     */
+    private Prize getPrizeForLotto(Lotto lotto) {
+        // 당첨 번호와 일치하는 숫자 개수 얻기
+        int matchedCount = countMatchedLottoNumber(lotto);
+        // 2등상인 경우
+        if (matchedCount == Prize.SECOND.getMatchedNumberCount() && isLottoMatchedBonusNumber(lotto)) {
+            return Prize.SECOND;
+        }
+        // 나머지 경우
+        return Prize.getMatchedPrize(matchedCount);
+    }
+
+    private void plusCount(Prize prize, Map<Prize, Integer> countResult) {
+        countResult.put(prize, countResult.getOrDefault(prize, 0) + 1);
+    }
+
+    private int countMatchedLottoNumber(Lotto lotto) {
+        int count = 0;
+        // 로또가 당첨 번호랑 몇 번 일치하는지 확인
+        for (int number : lotto.getNumbers()) {
+            if (isMatchedNumber(number)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    private boolean isMatchedNumber(int number) {
+        // 숫자가 당첨 번호에 해당하는지 확인
+        for (int winningNumber : winningLotto.getNumbers()) {
+            if (number == winningNumber) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean isLottoMatchedBonusNumber(Lotto lotto) {
+        // 로또가 보너스 숫자를 가지고 있는지 확인
+        for (int number : lotto.getNumbers()) {
+            if (number == bonusNumber.getValue()) {
+                return true;
+            }
+        }
+        return false;
     }
 }
