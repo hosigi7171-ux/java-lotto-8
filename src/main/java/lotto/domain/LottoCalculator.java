afce2e924afdb5d5ff9083557c19df55a5e938b2
hosigi7171-ux <hosigi7171@gmail.com>
2025-10-29 16:20:09 +0900

    feat(LottoCalculator): implement LottoCalculator to calculate purchasable Lotto count

--- src/main/java/lotto/domain/LottoCalculator.java
+++ src/main/java/lotto/domain/LottoCalculator.java
@@ -1,4 +1,9 @@
 package lotto.domain;
 
+import static lotto.constants.LottoConstants.PURCHASE_AMOUNT_DIVISOR;
+
 public class LottoCalculator {
+    public int calculatePurchasableLottoCount(int purchaseAmount) {
+        return purchaseAmount / PURCHASE_AMOUNT_DIVISOR;
+    }
 }
