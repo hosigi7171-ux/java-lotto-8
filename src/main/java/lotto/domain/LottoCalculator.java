7fb98281d8b285c5fe7f990dc546b93ac2ae1394
hosigi7171-ux <hosigi7171@gmail.com>
2025-10-29 20:50:42 +0900

    chore: remove unused LottoCalculator

--- src/main/java/lotto/domain/LottoCalculator.java
+++
@@ -1,9 +0,0 @@
-package lotto.domain;
-
-import static lotto.constants.LottoConstants.PURCHASE_AMOUNT_DIVISOR;
-
-public class LottoCalculator {
-    public int calculatePurchasableLottoCount(int purchaseAmount) {
-        return purchaseAmount / PURCHASE_AMOUNT_DIVISOR;
-    }
-}
