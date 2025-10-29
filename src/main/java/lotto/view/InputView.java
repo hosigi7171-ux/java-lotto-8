527979121bfa6dea5f03979cb6a8555f498af926
hosigi7171-ux <hosigi7171@gmail.com>
2025-10-29 21:16:20 +0900

    feat(View): Add new methods to InputView and OutputView interfaces

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,4 +1,10 @@
 package lotto.view;
 
+import lotto.domain.PurchaseAmount;
+import lotto.domain.WinningNumbers;
+
 public interface InputView {
+    PurchaseAmount readPurchaseAmount();
+
+    WinningNumbers readWinningNumbers();
 }
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,4 +1,14 @@
 package lotto.view;
 
+import java.util.List;
+import java.util.Map;
+import lotto.domain.Lotto;
+import lotto.domain.Prize;
+
 public interface OutputView {
+    void printLottos(List<Lotto> lottos);
+
+    void printStatistics(Map<Prize, Integer> prizeCounter);
+
+    void printBenefitRate(double benefitRate);
 }
