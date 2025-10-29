362034404de53194d4cfb674e3f413c977d6c2bf
hosigi7171-ux <hosigi7171@gmail.com>
2025-10-29 16:29:46 +0900

    feat(ConsoleOutputView): implement lotto result display on console

--- src/main/java/lotto/view/ConsoleOutputView.java
+++ src/main/java/lotto/view/ConsoleOutputView.java
@@ -1,4 +1,14 @@
 package lotto.view;
 
+import java.util.List;
+import lotto.domain.Lotto;
+
 public class ConsoleOutputView implements OutputView{
+
+    public void printLottos(List<Lotto> lottos){
+        System.out.printf("%d개를 구매했습니다.%n", lottos.size());
+        for(Lotto lotto : lottos){
+            System.out.println(lotto);
+        }
+    }
 }
