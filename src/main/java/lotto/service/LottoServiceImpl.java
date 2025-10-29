237647656a28fce2d2c43aea3c9fbf9e428c5acc
hosigi7171-ux <hosigi7171@gmail.com>
2025-10-29 16:20:51 +0900

    feat(LottoService): implement a lottery creation

--- src/main/java/lotto/service/LottoServiceImpl.java
+++ src/main/java/lotto/service/LottoServiceImpl.java
@@ -1,4 +1,28 @@
 package lotto.service;
 
-public class LottoServiceImpl implements LottoService{
+import static lotto.constants.LottoConstants.LOTTO_NUMBER_COUNT;
+import static lotto.constants.LottoConstants.LOTTO_NUMBER_MAX;
+import static lotto.constants.LottoConstants.LOTTO_NUMBER_MIN;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+import lotto.domain.Lotto;
+
+public class LottoServiceImpl implements LottoService {
+    private final List<Lotto> lottos;
+
+    public LottoServiceImpl() {
+        lottos = new ArrayList<>();
+    }
+
+    public void makeLottos(int lottoCount) {
+        for (int index = 0; index < lottoCount; index++) {
+            lottos.add(makeLotto());
+        }
+    }
+
+    private Lotto makeLotto() {
+        return new Lotto(Randoms.pickUniqueNumbersInRange(LOTTO_NUMBER_MIN, LOTTO_NUMBER_MAX, LOTTO_NUMBER_COUNT));
+    }
 }
