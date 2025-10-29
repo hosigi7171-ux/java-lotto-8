08b8357cde192a6f7f18ce8d650f4315bf6bcbda
hosigi7171-ux <hosigi7171@gmail.com>
2025-10-29 15:15:49 +0900

    feat(Lotto): implement Lotto class with basic number validation

---
+++ src/main/java/lotto/constants/LottoConstants.java
@@ -0,0 +1,9 @@
+package lotto.constants;
+
+public class LottoConstants {
+    private LottoConstants(){}
+
+    public static final int LOTTO_NUMBER_MIN = 1;
+    public static final int LOTTO_NUMBER_MAX = 45;
+    public static final int LOTTO_NUMBER_COUNT = 6;
+}
--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,6 +1,12 @@
 package lotto.domain;
 
+import static lotto.constants.LottoConstants.LOTTO_NUMBER_COUNT;
+import static lotto.constants.LottoConstants.LOTTO_NUMBER_MAX;
+import static lotto.constants.LottoConstants.LOTTO_NUMBER_MIN;
+
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -11,10 +17,36 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+        validateNumbersCount(numbers);
+        validateNumbersInRange(numbers);
+        validateNotDuplicate(numbers);
+    }
+
+    private void validateNotDuplicate(List<Integer> numbers) {
+        Set<Integer> distinctNumbers = new HashSet<>(numbers);
+        if(distinctNumbers.size() != numbers.size()){
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복되면 안됩니다.");
+        }
+    }
+
+    private void validateNumbersCount(List<Integer> numbers) {
+        if (numbers.size() != LOTTO_NUMBER_COUNT) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void validateNumbersInRange(List<Integer> numbers) {
+        for (int number : numbers) {
+            validateNumberInRange(number);
+        }
+    }
+
+    private void validateNumberInRange(int number) {
+        throw new IllegalArgumentException(
+                String.format("[ERROR] 로또 숫자는 %d~%d까지 입니다.", LOTTO_NUMBER_MIN, LOTTO_NUMBER_MAX));
+    }
+
+    public List<Integer> getNumbers(){
+        return numbers;
+    }
 }
