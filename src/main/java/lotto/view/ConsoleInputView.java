f855503a2d9e26ede489c902a93b70362b63a02b
hosigi7171-ux <hosigi7171@gmail.com>
2025-10-30 12:13:33 +0900

    refactor(View): inputView 와 outputView 에서 쓰이던 출력 메시지를 상수화

--- src/main/java/lotto/view/ConsoleInputView.java
+++ src/main/java/lotto/view/ConsoleInputView.java
@@ -9,21 +9,24 @@ import lotto.domain.PurchaseAmount;
 import lotto.domain.WinningNumbers;
 
 public class ConsoleInputView implements InputView {
+    private static final String PROMPT_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
+    private static final String PROMPT_WINNING_NUMBER = "당첨 번호를 입력해 주세요.";
+    private static final String PROMPT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
 
     public PurchaseAmount readPurchaseAmount() {
-        System.out.println("구입금액을 입력해 주세요.");
+        System.out.println(PROMPT_PURCHASE_AMOUNT);
         String input = Console.readLine();
         return new PurchaseAmount(input);
     }
 
     public WinningNumbers readWinningNumbers(){
         // 로또 당첨 번호 읽기
-        System.out.println("당첨 번호를 입력해 주세요.");
+        System.out.println(PROMPT_WINNING_NUMBER);
         Lotto winningLotto = readLotto();
         System.out.println();
 
         // 보너스 번호 읽기
-        System.out.println("보너스 번호를 입력해 주세요.");
+        System.out.println(PROMPT_BONUS_NUMBER);
         BonusNumber bonusNumber = readBonusNumber();
         System.out.println();
 
--- src/main/java/lotto/view/ConsoleOutputView.java
+++ src/main/java/lotto/view/ConsoleOutputView.java
@@ -6,26 +6,32 @@ import lotto.domain.Lotto;
 import lotto.domain.Prize;
 
 public class ConsoleOutputView implements OutputView {
+    private static final String PROMPT_PURCHASED_LOTTO_COUNT_FORMAT = "%d개를 구매했습니다.%n";
+    private static final String PROMPT_STATISTICS_TITLE = "당첨 통계";
+    private static final String PROMPT_SEPARATOR = "---";
+    private static final String PROMPT_MATCHING_FORMAT = "%d개 일치 (%,d원) - %d개%n";
+    private static final String PROMPT_BENEFIT_RATE_FORMAT = "총 수익률은 %.1f%%입니다.%n";
+
 
     public void printLottos(List<Lotto> lottos) {
-        System.out.printf("%d개를 구매했습니다.%n", lottos.size());
+        System.out.printf(PROMPT_PURCHASED_LOTTO_COUNT_FORMAT, lottos.size());
         for (Lotto lotto : lottos) {
             System.out.println(lotto);
         }
     }
 
     public void printStatistics(Map<Prize, Integer> prizeCounter) {
-        System.out.println("당첨 통계");
-        System.out.println("---");
+        System.out.println(PROMPT_STATISTICS_TITLE);
+        System.out.println(PROMPT_SEPARATOR);
 
         for (Prize prize : Prize.values()) {
             int count = prizeCounter.get(prize);
-            System.out.printf("%d개 일치 (%,d원) - %d개%n", prize.getMatchedNumberCount(), prize.getPrizeMoney(), count);
+            System.out.printf(PROMPT_MATCHING_FORMAT, prize.getMatchedNumberCount(), prize.getPrizeMoney(), count);
         }
     }
 
     public void printBenefitRate(double benefitRate){
         // 소수점 둘째 자리에서 반올림
-        System.out.printf("총 수익률은 %.1f%%입니다.%n", benefitRate);
+        System.out.printf(PROMPT_BENEFIT_RATE_FORMAT, benefitRate);
     }
 }
