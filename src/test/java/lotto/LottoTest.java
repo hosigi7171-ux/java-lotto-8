e832dbbb456d7b9060ef2cfe9dc322861d630e58
hosigi7171-ux <hosigi7171@gmail.com>
2025-10-30 18:12:54 +0900

    refactor(LottoTest): 로또 번호 에러 메시지 테스트 보완

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,12 +1,16 @@
 package lotto;
 
 import java.util.Arrays;
+import lotto.constants.ErrorMessage;
 import lotto.domain.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static lotto.constants.LottoConstants.LOTTO_NUMBER_COUNT;
+import static lotto.constants.LottoConstants.LOTTO_NUMBER_MAX;
+import static lotto.constants.LottoConstants.LOTTO_NUMBER_MIN;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -26,23 +30,27 @@ class LottoTest {
     @Test
     void 로또_번호에_중복된_숫자가_있으면_예외가_발생한다() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorMessage.LOTTO_NUMBER_DUPLICATE.getMessage());
     }
 
     @Test
-    void 로또_번호의_개수가_6개가_넘어가면_예외가_발생한다() {
+    void 로또_번호의_개수가_6개가_아니면_예외가_발생한다() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(String.format(ErrorMessage.LOTTO_NUMBER_COUNT_MISMATCH.getMessage(), LOTTO_NUMBER_COUNT));
     }
 
     @Test
     void 로또_번호의_범위가_잘못되면_예외가_발생한다() {
-        assertThatThrownBy(() -> new Lotto(List.of(0, 1, 2, 3, 4))).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> new Lotto(List.of(0, 1, 2, 3, 4, 99))).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(String.format(ErrorMessage.LOTTO_NUMBER_NOT_IN_RANGE.getMessage(), LOTTO_NUMBER_MIN,
+                        LOTTO_NUMBER_MAX));
     }
 
     @Test
-    void 로또_contains_기능_확인(){
-        Lotto lotto = new Lotto(Arrays.asList(1,2,3,4,5,6));
+    void 로또_contains_기능_확인() {
+        Lotto lotto = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6));
 
         assertThat(lotto.contains(3)).isEqualTo(true);
         assertThat(lotto.contains(7)).isEqualTo(false);
